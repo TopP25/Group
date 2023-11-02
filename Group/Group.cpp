54fac6dc4d3952b5e960c552fec002722aa502ff
Igor Putyajkin <putyajkin99@gmail.com>
2023-11-02 17:27:55 +0300

    init_Sub_main_Stud

--- Group/Group.cpp
+++ Group/Group.cpp
@@ -2,10 +2,13 @@
 //
 
 #include <iostream>
-
+#include "Subject.h"
+using namespace std;
 int main()
 {
     std::cout << "Hello World!\n";
+
+
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
