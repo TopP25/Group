b22396d687147a3bc6ab6823a561644aa44d1034
Denis <denfill008@mail.ru>
2023-11-02 17:29:16 +0300

    init

---
+++ Group/Classes.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Subject
+{
+	string name;
+public:
+	Subject(string name1)
+	{
+		Subject::name = name1;
+	}
+	~Subject()
+	{
+	
+	}
+	Subject()
+	{
+		Subject::name = "undefined";
+	}
+	string getName()
+	{
+		return Subject::name;
+	}
+
+};
+
+
+class Student
+{
+
+public:
+
+};
